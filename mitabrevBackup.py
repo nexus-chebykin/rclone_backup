8fcf0a92d705490fe4e8a17b4e4f6de7de55f81d
Simon <chebykin.sp@phystech.edu>
2024-01-30 17:46:40 +0500

    Add requirements.txt and script for cron deployment

---
+++ mitabrevBackup.py
@@ -0,0 +1,20 @@
+import argparse
+import pathlib
+import subprocess
+
+workingDirectory = pathlib.Path('/root/MitabrevFolder/backupScriptWorkingDirectory')
+if not workingDirectory.is_dir():
+    workingDirectory.mkdir()
+    print("Created working directory")
+    subprocess.run("git clone https://github.com/nexus-chebykin/rclone_backup .", cwd=workingDirectory,
+                   shell=True)
+    print("Cloned the repository")
+else:
+    subprocess.run("git pull", cwd=workingDirectory, shell=True)
+    print("Pulled the changes")
+
+subprocess.run("python3.9 -m pip install --requirement requirements.txt --upgrade", cwd=workingDirectory, shell=True)
+print("Installed & updated libraries")
+print("Now running main")
+with open(workingDirectory / 'log.txt', 'w') as log_file:
+    subprocess.run(["python3.9", "-u", "main.py"], cwd=workingDirectory, text=True, stdout=log_file, stderr=subprocess.STDOUT)
